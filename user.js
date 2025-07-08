e8a4cbc15570505c03e569a95a81f43dc2f32237
z3770 <mail@ugnyachev.ru>
2025-07-08 10:59:18 +0300

    Update user.js

--- user.js
+++ user.js
@@ -10,7 +10,7 @@
 /****************************************************************************
  * BetterZen                                                                *
  * "Ex nihilo nihil fit"                                                    *
- * version: 137                                                             *
+ * version: 138                                                             *
  * url: https://github.com/yokoffing/Betterfox                              *
 ****************************************************************************/
 
@@ -18,7 +18,6 @@
  * SECTION: FASTFOX                                                         *
 ****************************************************************************/
 /** GFX ***/
-user_pref("gfx.canvas.accelerated.cache-items", 8192); // DEFAULT FF135+
 user_pref("gfx.canvas.accelerated.cache-size", 512);
 
 /** DISK CACHE ***/
@@ -38,6 +37,7 @@ user_pref("network.predictor.enable-prefetch", false);
  * SECTION: SECUREFOX                                                       *
 ****************************************************************************/
 /** TRACKING PROTECTION ***/
+// user_pref("browser.contentblocking.category", "strict");
 user_pref("browser.download.start_downloads_in_tmp_dir", true);
 
 /** OCSP & CERTS / HPKP ***/
@@ -98,11 +98,14 @@ user_pref("zen.view.experimental-rounded-view", false);
 //user_pref("dom.ipc.processPriorityManager.backgroundUsesEcoQoS", true);
 
 // PREF: disable new tab preload since they are off by default
-//user_pref("browser.newtab.preload", false);
+user_pref("browser.newtab.preload", false);
 
 // PREF: show Enhance Tracking Protection shield in URL bar
 // Currently bugged if you click to view what's blocked
 //user_pref("zen.urlbar.show-protections-icon", true);
+
+// PREF: Disable the Picture in picture pop-out when changing tabs
+//user_pref("media.videocontrols.picture-in-picture.enable-when-switching-tabs.enabled", false);
 
 /****************************************************************************
  * START: MY OVERRIDES                                                      *
@@ -121,6 +124,66 @@ user_pref("zen.view.experimental-rounded-view", false);
 // Then apply an example from Smoothfox
 // [3] https://github.com/yokoffing/Betterfox/blob/main/Smoothfox.js
 // Enter your scrolling overrides below this line:
+
+/* Firefox built-in default values */
+user_pref("apz.overscroll.enabled", false);
+user_pref("general.smoothScroll", true);
+user_pref("general.smoothScroll.msdPhysics.enabled", false);
+user_pref("general.smoothScroll.currentVelocityWeighting", "0.25");
+user_pref("general.smoothScroll.stopDecelerationWeighting", "0.4");
+user_pref("mousewheel.min_line_scroll_amount", 5);
+user_pref("general.smoothScroll.mouseWheel.durationMinMS", 50);
+user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 120);
+user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 1250);
+user_pref("general.smoothScroll.msdPhysics.regularSpringConstant", 1000);
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS", 12);
+user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant", 2000);
+user_pref("mousewheel.default.delta_multiplier_y", 100);
+
+
+/*Smoothfox */
+
+// /****************************************************************************************
+//  * OPTION: SHARPEN SCROLLING                                                           *
+// ****************************************************************************************/
+// // credit: https://github.com/black7375/Firefox-UI-Fix
+// // only sharpen scrolling
+// user_pref("apz.overscroll.enabled", true); // DEFAULT NON-LINUX
+// user_pref("general.smoothScroll", true); // DEFAULT
+// user_pref("mousewheel.min_line_scroll_amount", 10); // adjust this number to your liking; default=5
+// user_pref("general.smoothScroll.mouseWheel.durationMinMS", 80); // default=50
+// user_pref("general.smoothScroll.currentVelocityWeighting", "0.15"); // default=.25
+// user_pref("general.smoothScroll.stopDecelerationWeighting", "0.6"); // default=.4
+// // Firefox Nightly only:
+// // [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1846935
+// user_pref("general.smoothScroll.msdPhysics.enabled", false); // [FF122+ Nightly]
+
+// /****************************************************************************************
+//  * OPTION: INSTANT SCROLLING (SIMPLE ADJUSTMENT)                                       *
+// ****************************************************************************************/
+// // recommended for 60hz+ displays
+// user_pref("apz.overscroll.enabled", true); // DEFAULT NON-LINUX
+// user_pref("general.smoothScroll", true); // DEFAULT
+// user_pref("mousewheel.default.delta_multiplier_y", 275); // 250-400; adjust this number to your liking
+// // Firefox Nightly only:
+// // [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1846935
+// user_pref("general.smoothScroll.msdPhysics.enabled", false); // [FF122+ Nightly]
+
+// /****************************************************************************************
+//  * OPTION: SMOOTH SCROLLING                                                            *
+// ****************************************************************************************/
+// // recommended for 90hz+ displays
+// user_pref("apz.overscroll.enabled", true); // DEFAULT NON-LINUX
+// user_pref("general.smoothScroll", true); // DEFAULT
+// user_pref("general.smoothScroll.msdPhysics.enabled", true);
+// user_pref("mousewheel.default.delta_multiplier_y", 300); // 250-400; adjust this number to your liking
+
+/****************************************************************************************
+ * OPTION: NATURAL SMOOTH SCROLLING V3 [MODIFIED]                                      *
+****************************************************************************************/
+// credit: https://github.com/AveYo/fox/blob/cf56d1194f4e5958169f9cf335cd175daa48d349/Natural%20Smooth%20Scrolling%20for%20user.js
+// recommended for 120hz+ displays
+// largely matches Chrome flags: Windows Scrolling Personality and Smooth Scrolling
 user_pref("apz.overscroll.enabled", true); // DEFAULT NON-LINUX
 user_pref("general.smoothScroll", true); // DEFAULT
 user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 12);
@@ -133,14 +196,6 @@ user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant", 250);
 user_pref("general.smoothScroll.currentVelocityWeighting", "1");
 user_pref("general.smoothScroll.stopDecelerationWeighting", "1");
 user_pref("mousewheel.default.delta_multiplier_y", 300); // 250-400; adjust this number to your liking
-
 /****************************************************************************
  * END: BETTERFOX                                                           *
-****************************************************************************/
-/****************************************************************************
- * SECTION: NEBULA                                                       *
-****************************************************************************/
-
-/****************************************************************************
- * END: NEBULA                                                           *
 ****************************************************************************/
